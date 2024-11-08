c8a2434e15921a79c7edefb7774003f2452aef4e
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-11-08 16:53:47 +0530

    added disease creation validation

---
+++ HealthGuard.Application/Validators/DiseaseValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using HealthGuard.Application.DTOs.Disease;
+
+namespace HealthGuard.Application.Validators
+{
+    public class DiseaseValidator : AbstractValidator<CreateDiseaseDto>
+    {
+        public DiseaseValidator()
+        {
+            RuleFor(r => r.Name)
+                .NotEmpty()
+                .MinimumLength(3);
+
+            RuleFor(r => r.Description)
+                .NotEmpty()
+                .MinimumLength(10);
+
+            RuleFor(r => r.VaccineAvailable)
+                .NotNull();
+
+            RuleFor(r => r.Symptoms)
+                .NotEmpty();
+        }
+    }
+}
